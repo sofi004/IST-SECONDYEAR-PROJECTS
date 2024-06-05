97f1575b27798fe090cbe397b69bbc9da84d521c
ist1106194 <sophiapiteira@gmail.com>
2024-06-05 12:42:23 +0100

    Ex 6 PE

--- Probability&Statistics-Project/Question6.R
+++ Probability&Statistics-Project/Question6.R
@@ -1,22 +1,22 @@
 a <- 4
-n <- 30
-seed <- 1948
+n <- 40
+seed <- 1973
 num_samples <- 1000
 rate <- 1/a
 scale <- 1/4
 
-set.seed(1948)
+set.seed(1973)
 
 y_larger_than_90_count <- 0
 for (i in 1:num_samples) {
   Xs <- rexp(n, rate=rate)
   Y <- sum(Xs)
-  if (Y > 90) {
+  if (Y > 126) {
     y_larger_than_90_count <- y_larger_than_90_count + 1
   }
 }
 proportion <- y_larger_than_90_count / num_samples
-exact <- 1 - pgamma(90, 30, rate=1/4)
+exact <- 1 - pgamma(126, 40, rate=1/4)
 
 diff <- abs(proportion - exact)
 diff
